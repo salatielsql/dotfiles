b2c225b9b5ec37115ea5e6cef62b845e071b0022
Salatiel Queiroz <6747627+salatielsql@users.noreply.github.com>
2025-07-27 09:57:04 -0300

    (wezterm): update theme to catppuccin

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,13 +3,13 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 -- config.font = wezterm.font("Lilex")
 config.font_size = 16
-config.color_scheme = "Dracula (Official)"
-config.window_background_opacity = 0.95
-config.tab_bar_at_bottom = true
+config.color_scheme = "catppuccin-mocha"
+config.window_background_opacity = 0.97
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
 
